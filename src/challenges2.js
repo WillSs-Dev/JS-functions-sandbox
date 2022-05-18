45eddc4c960110ae3a3666969f06119d63339125
WillBCc <williansilva3dsi@hotmail.com>
2022-05-18 20:36:37 -0300

    challenge 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,30 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  if(array.length != 11){
+    return 'Array com tamanho incorreto.'
+  }else{
+    let verify = true
+    for (const key in array) {
+      let currentNumber = array[key]
+      let repetitions = 0
+      for (const i in array) {
+        if(currentNumber == array[i]){
+          repetitions += 1
+        }
+      }
+      if (repetitions >= 3 || array[key] < 0 || array[key] > 9) {
+        verify = false
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+    }
+    if(verify == true){
+      let rawNumber = array.join('')
+      let ddd = rawNumber.slice(0,2)
+      let firstHalf = rawNumber.slice(2,7)
+      let secondHalf = rawNumber.slice(7,11)
+      return `(${ddd}) ${firstHalf}-${secondHalf}`
+    }
+  }
 }
 
 // Desafio 12
