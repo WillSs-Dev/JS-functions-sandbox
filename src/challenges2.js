bf95cdb7800cb00824fed4d6054ade3536d31485
WillBCc <williansilva3dsi@hotmail.com>
2022-05-19 22:12:10 -0300

    challenge 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,27 +1,27 @@
 // Desafio 11
 function generatePhoneNumber(array) {
   if(array.length != 11){
-    return 'Array com tamanho incorreto.'
+    return 'Array com tamanho incorreto.';
   }else{
     
     for (const key in array) {
       let currentNumber = array[key]
-      let repetitions = 0
+      let repetitions = 0;
       for (const i in array) {
         if(currentNumber == array[i]){
-          repetitions += 1
+          repetitions += 1;
         }
       }
       if (repetitions >= 3 || array[key] < 0 || array[key] > 9) {
         
-        return 'não é possível gerar um número de telefone com esses valores'
+        return 'não é possível gerar um número de telefone com esses valores';
       }
     }
     
-    let rawNumber = array.join('')
-    let ddd = rawNumber.slice(0,2)
-    let firstHalf = rawNumber.slice(2,7)
-    let secondHalf = rawNumber.slice(7,11)
+    let rawNumber = array.join('');
+    let ddd = rawNumber.slice(0,2);
+    let firstHalf = rawNumber.slice(2,7);
+    let secondHalf = rawNumber.slice(7,11);
     return `(${ddd}) ${firstHalf}-${secondHalf}`
     
   }
@@ -52,16 +52,31 @@ function triangleCheck(lineA, lineB, lineC) {
     }
   }
 
-  // console.log(isTriangle);
-  return isTriangle
+  return isTriangle;
 
 }
-triangleCheck(10, 13, 2)
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(drinks) {
+  let drinksStr = drinks.split('');
+  let numbers = 0;
+  let output = '';
+
+  for (const key in drinksStr) {
+    if(drinksStr[key] > 0 && drinksStr[key] < 10){
+      let change = drinksStr[key];
+      let sum = parseInt(change);
+      numbers += sum;
+    }
+  }
+  if(numbers > 1){
+    output = `${numbers} copos de água`;
+  }else{
+    output = `${numbers} copo de água`;
+  }
+  return output
 }
+
 
 module.exports = {
   generatePhoneNumber,
