f6ab432f977f095aaa90ae779d6f11865e24cf56
WillSs-Dev <williansilva3dsi@hotmail.com>
2022-11-27 10:20:08 -0300

    removing unecessary else statement

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,59 +1,54 @@
 // Desafio 11
 function generatePhoneNumber(array) {
-  if(array.length != 11){
+  if (array.length != 11) {
     return 'Array com tamanho incorreto.';
-  }else{
-    
-    for (const key in array) {
-      let currentNumber = array[key]
-      let repetitions = 0;
-      for (const i in array) {
-        if(currentNumber == array[i]){
-          repetitions += 1;
-        }
-      }
-      if (repetitions >= 3 || array[key] < 0 || array[key] > 9) {
-        
-        return 'não é possível gerar um número de telefone com esses valores';
+  }
+  for (const key in array) {
+    let currentNumber = array[key];
+    let repetitions = 0;
+    for (const i in array) {
+      if (currentNumber == array[i]) {
+        repetitions += 1;
       }
     }
-    
-    let rawNumber = array.join('');
-    let ddd = rawNumber.slice(0,2);
-    let firstHalf = rawNumber.slice(2,7);
-    let secondHalf = rawNumber.slice(7,11);
-    return `(${ddd}) ${firstHalf}-${secondHalf}`
-    
+    if (repetitions >= 3 || array[key] < 0 || array[key] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
   }
+
+  let rawNumber = array.join('');
+  let ddd = rawNumber.slice(0, 2);
+  let firstHalf = rawNumber.slice(2, 7);
+  let secondHalf = rawNumber.slice(7, 11);
+  return `(${ddd}) ${firstHalf}-${secondHalf}`;
 }
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
   let isTriangle = false;
   let sides = [lineA, lineB, lineC];
-  for(let i = 0; i < 3; i += 1){
-
-    if(Math.abs(sides[i]) < Math.abs(sides[i + 1] + sides[i + 2]) && Math.abs(sides[i]) > Math.abs(sides[i + 1] - sides[i + 2])){
-      
+  for (let i = 0; i < 3; i += 1) {
+    if (
+      Math.abs(sides[i]) < Math.abs(sides[i + 1] + sides[i + 2])
+      && Math.abs(sides[i]) > Math.abs(sides[i + 1] - sides[i + 2])
+    ) {
       isTriangle = true;
-
-    }else if(Math.abs(sides[i]) < Math.abs(sides[i - 1] + sides[i - 2]) && Math.abs(sides[i]) > Math.abs(sides[i - 1] - sides[i - 2])){
-
+    } else if (
+      Math.abs(sides[i]) < Math.abs(sides[i - 1] + sides[i - 2])
+      && Math.abs(sides[i]) > Math.abs(sides[i - 1] - sides[i - 2])
+    ) {
       isTriangle = true;
-
-    }else if(Math.abs(sides[i]) < Math.abs(sides[i + 1] + sides[i - 1]) && Math.abs(sides[i]) > Math.abs(sides[i + 1] - sides[i - 1])){
-
+    } else if (
+      Math.abs(sides[i]) < Math.abs(sides[i + 1] + sides[i - 1])
+      && Math.abs(sides[i]) > Math.abs(sides[i + 1] - sides[i - 1])
+    ) {
       isTriangle = true;
-
-    }else{
-
+    } else {
       isTriangle = false;
-
     }
   }
 
   return isTriangle;
-
 }
 
 // Desafio 13
@@ -63,20 +58,19 @@ function hydrate(drinks) {
   let output = '';
 
   for (const key in drinksStr) {
-    if(drinksStr[key] > 0 && drinksStr[key] < 10){
+    if (drinksStr[key] > 0 && drinksStr[key] < 10) {
       let change = drinksStr[key];
       let sum = parseInt(change);
       numbers += sum;
     }
   }
-  if(numbers > 1){
+  if (numbers > 1) {
     output = `${numbers} copos de água`;
-  }else{
+  } else {
     output = `${numbers} copo de água`;
   }
-  return output
+  return output;
 }
-
 
 module.exports = {
   generatePhoneNumber,
